934db41f305a8ddf1742288df465b952f455b148
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-12 23:05:35 +0700

    add millisecond delay func and microsecond delay func

--- Core/Inc/timer.h
+++ Core/Inc/timer.h
@@ -8,8 +8,13 @@
 #ifndef INC_TIMER_H_
 #define INC_TIMER_H_
 #define TIM1_BASE_ADDR 0x40010000
-
 void TIM1_UP_TIM10_IRQHandler();
+
+void delay_microsec(uint32_t time);
+void delay_millisec(uint32_t time);
 void delay_sec(uint32_t time);
+void microsec_config();
+void millisec_config();
+void sec_config();
 void TIM_Init();
 #endif /* INC_TIMER_H_ */
--- Core/Src/timer.c
+++ Core/Src/timer.c
@@ -7,8 +7,29 @@
 #include "main.h"
 #include "clock.h"
 #include "timer.h"
-
 static int cnt = 0;
+
+void delay_microsec(uint32_t time)
+{
+	microsec_config();
+	cnt = 0;
+	while (cnt <= time);
+}
+
+void delay_millisec(uint32_t time)
+{
+	millisec_config();
+	cnt = 0;
+	while (cnt < time);
+}
+
+void delay_sec(uint32_t time)
+{
+	sec_config();
+	cnt = 0;
+	while (cnt < time);
+}
+
 void TIM1_UP_TIM10_IRQHandler()
 {
 	uint16_t* TIM1_SR  = (uint16_t*) (TIM1_BASE_ADDR + 0x10);
@@ -21,13 +42,54 @@ void TIM1_UP_TIM10_IRQHandler()
 	cnt++;
 }
 
-
-void delay_sec(uint32_t time)
+void microsec_config()
 {
-	cnt = 0;
-	while (cnt < time);
+	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
+	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
+	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
+	/* set CK_CNT = 1kHz */
+	*TIM1_PSC = 1 - 1;
+
+	/* set auto-reload value */
+	*TIM1_ARR = 32 - 1;
+
+	/* (force) generate update */
+	*TIM1_EGR |= 1 << 0;
 }
 
+void millisec_config()
+{
+	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
+	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
+	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
+	/* set CK_CNT = 1kHz */
+	*TIM1_PSC = 32 - 1;
+
+	/* set auto-reload value */
+	*TIM1_ARR = 1000 - 1;
+
+	/* (force) generate update */
+	*TIM1_EGR |= 1 << 0;
+}
+
+void sec_config()
+{
+	uint16_t* TIM1_PSC = (uint16_t*) (TIM1_BASE_ADDR + 0x28);
+	uint16_t* TIM1_ARR = (uint16_t*) (TIM1_BASE_ADDR + 0x2C);
+	uint16_t* TIM1_EGR = (uint16_t*) (TIM1_BASE_ADDR + 0x14);
+	/* set CK_CNT = 1kHz */
+	*TIM1_PSC = 32000 - 1;
+
+	/* set auto-reload value */
+	*TIM1_ARR = 1000 - 1;
+
+	/* (force) generate update */
+	*TIM1_EGR |= 1 << 0;
+}
+
+/**
+ * @brief  this function is used to initial timer1
+ */
 void TIM_Init()
 {
 	APB2_clock_enable(APB2_TIM1);
