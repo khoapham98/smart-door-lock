31f4a3eda2bff7bd8ebc0c653bbbf3508ddd1b66
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-18 15:36:05 +0700

    Implement RFID mode switch using button input

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -2,32 +2,43 @@
 #include "clock.h"
 #include "timer.h"
 #include "MFRC522.h"
+#include "button.h"
 
-uint8_t str[16];
 uint8_t uid_ls[MAX_UIDs][4];
-int add, rm;
 extern uint8_t uid_cnt;
+extern uint8_t button_state;
 int main()
 {
 	RCC_Init();
 	TIM_Init();
 	SPI_Init();
+	BUTTON_Init();
 	MFRC522_Init();
 
 	while (1)
 	{
-		if (MFRC522_Request(PICC_REQA, str) == MI_OK)
+		if (MFRC522_IsTagPresent())
 		{
-			if (uid_cnt < 2)
+			switch (button_state)
 			{
-				add = MFRC522_CheckAndStoreUID(uid_ls);
-			}
-			else
-			{
-				rm = MFRC522_RemoveUID(uid_ls);
+				case IDLE:
+					if (MFRC522_IsValidUID(uid_ls))
+					{
+						// control motor to open the door
+					}
+					break;
+				case ADD:
+					MFRC522_CheckAndStoreUID(uid_ls);
+					break;
+				case RM:
+					MFRC522_RemoveUID(uid_ls);
+					break;
+				default:
+					break;
 			}
 		}
-		delay_millisec(100);
+		delay_millisec(200);
 	}
+
 	return 0;
 }
