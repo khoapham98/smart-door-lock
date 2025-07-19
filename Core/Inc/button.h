d59c5f9a6f5221f5dee1cdf21d6d1f8cf0cba876
anhkh04 <phamanhkhoa9803@gmail.com>
2025-07-19 11:32:13 +0700

    upd button lib: implement function to select mode for RFID reader

--- Core/Inc/button.h
+++ Core/Inc/button.h
@@ -7,13 +7,16 @@
 
 #ifndef INC_BUTTON_H_
 #define INC_BUTTON_H_
-#define IDLE 0x00
-#define ADD  0x01
-#define RM   0x02
-#define PB	 0x01
-
-#define 	GPIOB_BASE_ADDR 	0x40020400
-#define 	EXTI_BASE_ADDR  	0x40013C00
-#define 	SYSCFG_BASE_ADDR 	0x40013800
+#define NORMAL 	0x00
+#define ADD  	0x01
+#define REMOVE  0x02
+#define PB	 	0x01
+#define GPIOB_BASE_ADDR 	0x40020400
+#define EXTI_BASE_ADDR  	0x40013C00
+#define SYSCFG_BASE_ADDR 	0x40013800
+uint8_t selectMODE();
+uint8_t isPressed();
 void BUTTON_Init();
+void ButtonInit();
+char AisPressed();
 #endif /* INC_BUTTON_H_ */
--- Core/Src/button.c
+++ Core/Src/button.c
@@ -6,51 +6,94 @@
  */
 #include "main.h"
 #include "clock.h"
+#include "timer.h"
 #include "button.h"
+uint8_t button_state = NORMAL;
+int button_cnt = 0;
 
-uint8_t button_state = IDLE;
-
+/**
+ * One click 	: Add UID
+ * Double click : Remove UID
+ * Press & hold : IDLE mode (open the door)
+ */
 void EXTI1_IRQHandler()
 {
 	uint32_t* EXTI_PR = (uint32_t*) (EXTI_BASE_ADDR + 0x14);
-	button_state = ADD;
+	button_state = selectMODE();
 	*EXTI_PR |= 1 << 1;
 }
 
-void EXTI2_IRQHandler()
+static void delay_ms(uint32_t ms)
 {
-	uint32_t* EXTI_PR = (uint32_t*) (EXTI_BASE_ADDR + 0x14);
-	button_state = RM;
-	*EXTI_PR |= 1 << 2;
+	for (uint32_t j = 0; j < ms; j++)
+	{
+		for (volatile uint32_t i = 0; i < 8000; i++);
+	}
+}
+
+uint8_t isPressed()
+{
+	uint32_t* GPIOB_IDR = (uint32_t*) (GPIOB_BASE_ADDR + 0x10);
+	if (((*GPIOB_IDR >> 1) & 1) == 1)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+uint8_t selectMODE()
+{
+	uint8_t mode;
+	int time = 1;
+	while (isPressed())
+	{
+		button_cnt++;
+		delay_ms(50);
+		if (isPressed()) time = 2;
+	}
+
+	if (button_cnt > 0 && button_cnt <= 4)
+	{
+		if (time == 1)
+		{
+			mode = ADD;
+		}
+		else if (time == 2)
+		{
+			mode = REMOVE;
+		}
+	}
+	else
+	{
+		mode = NORMAL;
+	}
+	button_cnt = 0;
+	return mode;
 }
 
 /**
- * @brief  Configure PB1 and PB2 for buttons and use external interrupt to detect when the button is pressed
+ * @brief  Configure PB1 for buttons and use external interrupt to detect when the button is pressed
  */
 void BUTTON_Init()
 {
-	AHB1_clock_enable(AHB1_GPIOB);
 	APB2_clock_enable(APB2_SYSCFG);
 	uint32_t* GPIOB_MODER = (uint32_t*) (GPIOB_BASE_ADDR + 0x00);
-	uint32_t* GPIOB_PUPDR = (uint32_t*) (GPIOB_BASE_ADDR + 0x0C);
 	uint32_t* EXTI_IMR    = (uint32_t*) (EXTI_BASE_ADDR  + 0x00);
 	uint32_t* EXTI_RTSR   = (uint32_t*) (EXTI_BASE_ADDR  + 0x08);
 	uint32_t* NVIC_ISER0  = (uint32_t*) (0xE000E100);
 	uint32_t* SYSCFG_EXTICR1 = (uint32_t*) (SYSCFG_BASE_ADDR + 0x08);
 
-	/* configure PB1 & PB2 as INPUT */
-	*GPIOB_MODER &= ~((0b11 << (1 * 2)) | (0b11 << (2 * 2)));
+	/* configure PB1 as INPUT */
+	*GPIOB_MODER &= ~(0b11 << (1 * 2));
 
-	/* configure pull-up resister for PB1 & PB2 */
-	*GPIOB_PUPDR &= ~((0b11 << (1 * 2)) | (0b11 << (2 * 2)));
-	*GPIOB_PUPDR |= (0b01 << (1 * 2)) | (0b01 << (2 * 2));
-
-	/* select Port B for EXTI interrupt */
-	*SYSCFG_EXTICR1 |= (PB << 4) | (PB << 8);
+	/* select Port B for EXTI interrupt line 1 */
+	*SYSCFG_EXTICR1 |= (PB << 4);
 
 	/* configure EXTI & NVIC */
-	*EXTI_RTSR  |= (1 << 1) | (1 << 2);
-	*EXTI_IMR   |= (1 << 1) | (1 << 2);
-	*NVIC_ISER0 |= (1 << 7) | (1 << 8);
+	*EXTI_RTSR  |= (1 << 1);
+	*EXTI_IMR   |= (1 << 1);
+	*NVIC_ISER0 |= (1 << 7);
 }
-
